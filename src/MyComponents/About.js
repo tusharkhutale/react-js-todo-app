4ea9583e633ce651145fbd6f3f55ced967ffa83e
tusharkhutale <57614382+tusharkhutale@users.noreply.github.com>
2022-11-05 14:19:14 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component.
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sint error ullam vitae dolorum dignissimos quod similique exercitationem eos quisquam quo adipisci hic illo reprehenderit, numquam iure consectetur quas perferendis.
+        </p>
+    </div>
+  )
+}
---
+++ src/MyComponents/AddTodo.js
@@ -0,0 +1,36 @@
+
+import React, {useState} from 'react';
+
+export const AddTodo = (props) => {
+    const [title, setTitle] = useState("");
+    const [desc, setDesc] = useState("");
+
+    const submit = (e) =>{
+        e.preventDefault();
+        if(!title || !desc){
+            alert("Title or Description cannot be blank");
+        }
+        else{
+            props.addTodo(title, desc);
+            setTitle("");
+            setDesc("");
+        }
+       
+    }
+    return (
+        <div className="container my-3">
+            <h3>Add a Todo</h3>
+            <form onSubmit={submit}>
+               <div className="mb-3">
+                <label htmlFor="title" className="form-label">Todo Title</label>
+                <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control" id="title" placeholder="Example input placeholder" />
+               </div>
+                <div className="mb-3">
+                    <label htmlFor="desc" className="form-label">Todo Description</label>
+                    <input type="text" value={desc} onChange={(e)=>{setDesc(e.target.value)}} className="form-control" id="desc" placeholder="Another input placeholder" />
+                </div>
+                <button type="submit" className="btn btn btn-success">Add Todo</button>
+                </form>
+         </div>
+                )
+}
